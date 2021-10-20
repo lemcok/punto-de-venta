16906776d3b08c3f7c6b090453ae0501c29b37c4
Ricardo Flores <rik.fm1809@gmail.com>
2021-10-20 13:21:33 -0500

    Implement, getOneProduct and UpdateProduct

--- src/MainScreen/main.js
+++ src/MainScreen/main.js
@@ -5,8 +5,8 @@ const { connectDB } = require('../config/connectDB');
 let win;
 function createWindow () {
     win = new BrowserWindow({
-      width: 800,
-      height: 600,
+      width: 920,
+      height: 650,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
@@ -18,7 +18,7 @@ function createWindow () {
     win.loadFile('src/MainScreen/main.html')
 } 
 
-/**get all Products */
+/**Get all Products */
 ipcMain.handle('get', () => {
     getProducts()
 })
@@ -28,17 +28,40 @@ const getProducts = async() => {
     win.webContents.send('products', products)
 }
 
+/**Get One Product */
+ipcMain.handle('get_one', (e, idproduct) => {
+    getOneProduct(idproduct)    
+});
+const getOneProduct = async(id = 2) => {
+    const db = await connectDB()
+    let sql = "SELECT * FROM products WHERE id = ?"
+    const product = await db.query( sql, id )
+    win.webContents.send('product', product[0])
+}
+
 /**Register one product */
-ipcMain.handle('add', ( e, obj )=> {
+ipcMain.handle('addProduct', ( e, obj )=> {
     addProduct( obj )
 })
-
 const addProduct = async( product ) => {
     const db = await connectDB()
     const sql = 'INSERT INTO products SET ?'
     await db.query( sql, product )
     getProducts()
 }
+
+/**Update one product */
+ipcMain.handle('updateProduct', (event, obj) => {
+    updateProduct(obj)    
+});
+const updateProduct = async( product ) => {
+    const { id, name, price, description } = product;
+    const db = await connectDB()
+    const sql = 'UPDATE products SET name=?, price=?, description=? WHERE id=?'
+    await db.query( sql, [name, price, description, id] )
+    getProducts()
+}
+
 
 
 
--- src/MainScreen/main.render.js
+++ src/MainScreen/main.render.js
@@ -20,39 +20,96 @@ ipcRenderer.on('products', (e, products) => {
                 <td>${element.name}</td>
                 <td>S/. ${element.price.toFixed(2)}</td>
                 <td>${element.description}</td>
+                <td>
+                    <button 
+                        class="btn btn-info"
+                        id="btnEdit"
+                        value="${element.id}"
+                    >
+                        Edit
+                    </button>
+                </td>
+                <td>
+                    <button 
+                        class="btn btn-danger"
+                        value="${element.id}"
+                    >
+                        Delete
+                    </button>
+                </td>
             </tr>
         `
+        
     });
     mylist.innerHTML = template
+
+    const btnEdit = document.querySelectorAll(".btn-info")
+    btnEdit.forEach( btn => {
+        btn.addEventListener('click', renderGetOneProduct)
+    } )
 })
 
-/**
- * get values from form inputs
- */
+/** Register Product */
 const productForm = document.getElementById('productForm');
 const name = document.getElementById('name');
 const price = document.getElementById('price');
 const description = document.getElementById('description');
 
+//---fn limpiar Inputs
+const clearInputs = () => {
+    idProduct.value =""
+    name.value = ""
+    price.value = ""
+    description.value = ""
+}
+
 productForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    renderAddProduct()
+    if( name.value === '') return;
+    
+    if (idProduct.value.length > 0) {
+        renderUpdateProduct()
+        clearInputs()
+    }else {
+        renderAddProduct()
+        clearInputs()
+    }
+
 })
 
-ipcRenderer.on('product', (event, result) => {
-    // idproduct.value = result.id
-    name.value = result.name
-    price.value = result.price
-    price.value = result.description
-});
 const renderAddProduct = async() => {
     const newProduct = {
         name: name.value,
         price: price.value,
         description: description.value,
     }
-    name.value= "";
-    price.value= "";
-    description.value= "";
-    await ipcRenderer.invoke('add', newProduct)
+    await ipcRenderer.invoke('addProduct', newProduct)
 }
+
+
+/**  Get one product*/
+const idProduct = document.getElementById('idProduct'); 
+ipcRenderer.on('product', (event, result) => {
+    idProduct.value = result.id
+    name.value = result.name
+    price.value = result.price
+    description.value = result.description
+});
+const renderGetOneProduct = async(e) => {
+    const idProduct = e.target.value
+    await ipcRenderer.invoke('get_one', idProduct)
+}
+
+/**Update Product */
+
+const renderUpdateProduct = async() => {
+    const newProduct = {
+        id: idProduct.value,
+        name: name.value,
+        price: price.value,
+        description: description.value,
+    }
+    await ipcRenderer.invoke('updateProduct', newProduct)
+}
+
+
